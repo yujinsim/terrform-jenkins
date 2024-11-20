ee8aadb0e2eb71abc811fb1095d0302928c737df
yujin <yujin@example.com>
2024-11-20 14:27:12 +0900

    vpc

--- 03_ig.tf
+++
@@ -1,7 +0,0 @@
-resource "aws_internet_gateway" "yjsim_ig" {
-  vpc_id = aws_vpc.yjsim_vpc.id
-
-  tags = {
-    Name = "yjsim-ig"
-  }
-}
--- 05_rtas.tf
+++
@@ -1,9 +0,0 @@
-resource "aws_route_table_association" "yjsim_rtas_puba" {
-  subnet_id      = aws_subnet.yjsim_puba.id
-  route_table_id = aws_route_table.yjsim_public_rt.id
-}
-
-resource "aws_route_table_association" "yjsim_rtas_pubc" {
-  subnet_id      = aws_subnet.yjsim_pubc.id
-  route_table_id = aws_route_table.yjsim_public_rt.id
-}
